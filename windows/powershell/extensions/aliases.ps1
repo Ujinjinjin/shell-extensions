3ba98ae2e2ec6befa8ec61f34175e9a39f5572dd
Camille Galladjov <ujinjinjin@outlook.com>
2019-09-27 00:07:41 +0300

    sup yo

--- windows/powershell/extensions/aliases.ps1
+++ windows/powershell/extensions/aliases.ps1
@@ -4,3 +4,4 @@ function work { set-location "d:/gallk/work" }
 function training { set-location "d:/gallk/work/training" }
 function games { set-location "d:/gallk/work/games" }
 function web { set-location "d:/gallk/work/web" }
+fkawopkfpo
